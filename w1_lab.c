5d744e431d2e43e0defb6bd22b2dccc3018b1f68
DrChinaWhite <drhawkins@mta.ca>
2020-10-02 23:48:25 -0400

    Add files via upload

---
+++ w1_lab.c
@@ -0,0 +1,19 @@
+/* Name: Dale Hawkins Student ID: 116367202 Email: dhawkins8@myseneca.ca */
+
+
+#include <stdio.h>	// C language module providing Input/Output facilities
+
+
+int main(void)
+{
+	// declaring variables
+	char sentence[] = "Welcome to C Programming";
+	char border[] = "***";
+
+
+	//printing the statement
+	printf("%s %s %s\n", border, sentence, border);
+
+
+	return 0;
+}
